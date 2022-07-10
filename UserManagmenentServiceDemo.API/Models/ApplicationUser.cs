091150295d22de2f2db2dfc18e61741e57257b6e
Simuzeche Kaluwa <simuzechek@gmail.com>
2022-07-10 12:21:09 +0200

    Feat: Added Application User and Connection String

---
+++ UserManagmenentServiceDemo.API/Models/ApplicationUser.cs
@@ -0,0 +1,12 @@
+﻿using Microsoft.AspNetCore.Identity;
+
+namespace UserManagmenentServiceDemo.API.Models
+{
+    public class ApplicationUser : IdentityUser
+    {
+        public string FirstName { get; set; } = string.Empty;
+        public string LastName { get; set; } = string.Empty;
+        public bool ActivationStatus { get; set; } = true;
+        public DateTimeOffset CreationDate { get; set; } = DateTimeOffset.Now;
+    }
+}
