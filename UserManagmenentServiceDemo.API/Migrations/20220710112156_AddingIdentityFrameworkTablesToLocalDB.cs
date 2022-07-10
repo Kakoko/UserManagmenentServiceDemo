787b4a3c58d9732cc86722088d7a4fa01b071b08
Simuzeche Kaluwa <simuzechek@gmail.com>
2022-07-10 13:29:45 +0200

    Feat: Added Migration

---
+++ UserManagmenentServiceDemo.API/Migrations/20220710112156_AddingIdentityFrameworkTablesToLocalDB.cs
@@ -0,0 +1,19 @@
+﻿using Microsoft.EntityFrameworkCore.Migrations;
+
+#nullable disable
+
+namespace UserManagmenentServiceDemo.API.Migrations
+{
+    public partial class AddingIdentityFrameworkTablesToLocalDB : Migration
+    {
+        protected override void Up(MigrationBuilder migrationBuilder)
+        {
+
+        }
+
+        protected override void Down(MigrationBuilder migrationBuilder)
+        {
+
+        }
+    }
+}
