0ee1d21a5c8b3989bc3f09c7b2224359241ce094
Simuzeche Kaluwa <simuzechek@gmail.com>
2022-07-11 10:29:42 +0200

    Feat: Added Model that will be used for registering admins

---
+++ UserManagmenentServiceDemo.API/Models/User/RegisterAdminModelUsers.cs
@@ -0,0 +1,21 @@
+﻿using System.ComponentModel.DataAnnotations;
+
+namespace UserManagmenentServiceDemo.API.Models.User
+{
+    public class RegisterAdminModelUsers
+    {
+        [Required(ErrorMessage = "First Name is required")]
+        public string FirstName { get; set; } = string.Empty;
+
+        [Required(ErrorMessage = "Last  Name is required")]
+        public string LastName { get; set; } = string.Empty;
+
+        [EmailAddress]
+        [Required(ErrorMessage = "Email is required")]
+        public string Email { get; set; } = string.Empty;
+
+        [Phone]
+        [Required(ErrorMessage = "Phone Number is required")]
+        public string Phone { get; set; } = string.Empty;
+    }
+}
