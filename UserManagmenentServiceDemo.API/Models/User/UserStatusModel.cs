4753537a7ff44304116871dd617afb7399fd3aaf
Simuzeche Kaluwa <simuzechek@gmail.com>
2022-07-13 19:36:53 +0200

    Feat: Added UserStatus model to aid user activation or deactivation

---
+++ UserManagmenentServiceDemo.API/Models/User/UserStatusModel.cs
@@ -0,0 +1,9 @@
+﻿namespace UserManagmenentServiceDemo.API.Models.User
+{
+    public class UserStatusModel
+    {
+        public string Username { get; set; } = string.Empty;
+        public bool Flag { get; set; }
+    }
+}
+    
