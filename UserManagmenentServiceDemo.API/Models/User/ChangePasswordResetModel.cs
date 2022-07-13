507b1bc69b2e7a7a100f2f55b533c9205b7afe3b
Simuzeche Kaluwa <simuzechek@gmail.com>
2022-07-13 14:49:43 +0200

    Feat: Added Password CHange Model

---
+++ UserManagmenentServiceDemo.API/Models/User/ChangePasswordResetModel.cs
@@ -0,0 +1,9 @@
+﻿namespace UserManagmenentServiceDemo.API.Models.User
+{
+    public class ChangePasswordResetModel
+    {
+        public string Username { get; set; } = string.Empty;
+        public string NewPassword { get; set; } = string.Empty;
+        public string CurrentPassword { get; set; } = string.Empty;
+    }
+}
