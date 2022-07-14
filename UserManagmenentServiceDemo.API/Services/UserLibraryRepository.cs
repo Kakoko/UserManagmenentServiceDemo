cc92e53ebbc58cef41de6389557f99fb25d76b6c
Simuzeche Kaluwa <simuzechek@gmail.com>
2022-07-14 15:11:20 +0200

    Feat: Injected Identity Framework and Entity Framework Application context

--- UserManagmenentServiceDemo.API/Services/UserLibraryRepository.cs
+++ UserManagmenentServiceDemo.API/Services/UserLibraryRepository.cs
@@ -1,10 +1,22 @@
-﻿using UserManagmenentServiceDemo.API.Models;
+﻿using Microsoft.AspNetCore.Identity;
+using UserManagmenentServiceDemo.API.Context;
+using UserManagmenentServiceDemo.API.Models;
 using UserManagmenentServiceDemo.API.Models.User;
 
 namespace UserManagmenentServiceDemo.API.Services
 {
     public class UserLibraryRepository : IUserLibraryRepository
     {
+
+        private readonly ApplicationDbContext _context;
+        private readonly UserManager<ApplicationUser> _userManager;
+        private readonly RoleManager<IdentityRole> _roleManager;
+        public UserLibraryRepository(ApplicationDbContext context, UserManager<ApplicationUser> userManager, RoleManager<IdentityRole> roleManager)
+        {
+            _context = context ?? throw new ArgumentNullException(nameof(context));
+            _userManager = userManager;
+            _roleManager = roleManager;
+        }
         public Task<UserInfoModel> EditUser(UserInfoModel user)
         {
             throw new NotImplementedException();
