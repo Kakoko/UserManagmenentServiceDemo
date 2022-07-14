4105bd0f22541dfd236dd1bc7c03233a805f957b
Simuzeche Kaluwa <simuzechek@gmail.com>
2022-07-14 14:56:36 +0200

    Feat: Created user repositories for CRUD operations

---
+++ UserManagmenentServiceDemo.API/Services/IUserLibraryRepository.cs
@@ -0,0 +1,13 @@
+﻿using UserManagmenentServiceDemo.API.Models;
+
+namespace UserManagmenentServiceDemo.API.Services
+{
+    public interface IUserLibraryRepository
+    {
+        Task<List<ApplicationUser>> GetAllUsers(string flag);
+        Task<ApplicationUser> GetUserByUsername(string username);
+        Task<ApplicationUser> EditUser(ApplicationUser user);
+
+
+    }
+}
---
+++ UserManagmenentServiceDemo.API/Services/UserLibraryRepository.cs
@@ -0,0 +1,22 @@
+﻿using UserManagmenentServiceDemo.API.Models;
+
+namespace UserManagmenentServiceDemo.API.Services
+{
+    public class UserLibraryRepository : IUserLibraryRepository
+    {
+        public Task<ApplicationUser> EditUser(ApplicationUser user)
+        {
+            throw new NotImplementedException();
+        }
+
+        public Task<List<ApplicationUser>> GetAllUsers(string flag)
+        {
+            throw new NotImplementedException();
+        }
+
+        public Task<ApplicationUser> GetUserByUsername(string username)
+        {
+            throw new NotImplementedException();
+        }
+    }
+}
