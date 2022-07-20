bd185608c621115ac016b52336a716a262051ffa
Simuzeche Kaluwa <simuzechek@gmail.com>
2022-07-20 11:13:25 +0200

    Feat: Implmented Get Resource

--- UserManagementServiceDemo.Client/Services/CRUDService.cs
+++ UserManagementServiceDemo.Client/Services/CRUDService.cs
@@ -8,9 +8,28 @@ namespace UserManagementServiceDemo.Client.Services
 {
     public class CRUDService : IIntegrationService
     {
-        public Task Run()
+
+        private static HttpClient _httpClient = new HttpClient();
+
+
+        public CRUDService()
         {
-            throw new NotImplementedException();
+            //set up HTTPClient instance
+            _httpClient.BaseAddress = new Uri("https://localhost:44386");
+            _httpClient.Timeout = new TimeSpan(0, 0, 30); 
+
         }
+        public async Task Run()
+        {
+            await GetResource();
+        }
+        
+
+        public async Task GetResource()
+        {
+            var response = await _httpClient.GetAsync("api/Report/users");
+            response.EnsureSuccessStatusCode();
+        }
+
     }
 }
